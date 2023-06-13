c5f3c228b0d51c901688f5076b0843fc90923e0e
Florin Alexandru Sirbu <florinsirbu280599@gmail.com>
2023-06-13 22:22:33 +0200

    Add files via upload

---
+++ LogInandRegistration Spring Boot/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@RequestParam String username, @RequestParam String password, Model model) {
+        User user = userRepository.findByUsername(username);
+
+        if (user != null && user.getPassword().equals(password)) {
+            // Username and password are correct
+            model.addAttribute("user", user);
+            return "success"; // Redirect to the success page
+        } else {
+            // Username and password are incorrect
+            return "failure"; // Redirect to the failure page
+        }
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(User user) {
+        userRepository.save(user);
+        return "redirect:/user/login";
+    }
+}
---
+++ LogInandRegistration Spring Boot/demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,71 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String vorname;
+    private String name;
+    private String username;
+    private String password;
+
+    // Default constructor (required by JPA)
+    public User() {
+    }
+
+    // Constructor with all fields
+    public User(String vorname, String name, String username, String password) {
+        this.vorname = vorname;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
